5a9bb599c45a8ec0552a59068b0089d5ba917452
Dileepa Jayathilaka <DileepaJ@99x.lk>
2018-05-08 18:15:44 +0530

    Initial Commit

---
+++ FlexibleComputerLanguage/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FlexibleComputerLanguage
+//
+//  Created by Dileepa Jayathilaka on 5/8/18.
+//  Copyright (c) 2018 Dileepa Jayathilaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
