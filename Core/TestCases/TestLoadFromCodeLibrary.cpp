9f0f3869f93279223a66d13da0284093b1e3cb0f
Dileepa Jayathilaka <DileepaJ@99x.lk>
2018-05-24 14:46:34 +0530

    Added capability to load from a code library 2 new additions in Def file 	LOAD_FROM_CODE_LIBRARY 	CODE_LIBRARY_FILE Refer the test case LoadFromCodeLibrary

---
+++ Core/TestCases/TestLoadFromCodeLibrary.cpp
@@ -0,0 +1,9 @@
+//
+//  TestLoadFromCodeLibrary.cpp
+//  FlexibleComputerLanguage
+//
+//  Created by Dileepa Jayathilaka on 5/24/18.
+//  Copyright (c) 2018 Dileepa Jayathilaka. All rights reserved.
+//
+
+#include "TestLoadFromCodeLibrary.h"
---
+++ Core/TestCases/TestLoadFromCodeLibrary.h
@@ -0,0 +1,14 @@
+//
+//  TestLoadFromCodeLibrary.h
+//  FlexibleComputerLanguage
+//
+//  Created by Dileepa Jayathilaka on 5/24/18.
+//  Copyright (c) 2018 Dileepa Jayathilaka. All rights reserved.
+//
+
+#ifndef __FlexibleComputerLanguage__TestLoadFromCodeLibrary__
+#define __FlexibleComputerLanguage__TestLoadFromCodeLibrary__
+
+#include <iostream>
+
+#endif /* defined(__FlexibleComputerLanguage__TestLoadFromCodeLibrary__) */
