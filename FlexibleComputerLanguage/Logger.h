82bbcab8cceafcdd9f32f17684b7101adda57b8d
murtazasmart <murtazasmart@hotmail.com>
2018-08-28 16:05:11 +0530

    File formatted

--- FlexibleComputerLanguage/Logger.h
+++ FlexibleComputerLanguage/Logger.h
@@ -8,9 +8,10 @@
 #include <thread>
 #include "easylogging++.h"
 
-class Logger {
+class Logger
+{
 
-public:
+  public:
     static void ConfigureLogger()
     {
         // Console logger with color
@@ -19,24 +20,25 @@ public:
         defaultConf.setToDefault();
         defaultConf.setGlobally(el::ConfigurationType::Filename, "logs/info.%datetime{%Y-%M-%d_%H-%m-%s}.log");
         el::Loggers::reconfigureLogger("default", defaultConf);
-        std::thread logRotatorThread([](){
+        std::thread logRotatorThread([]() {
             const std::chrono::seconds wakeUpDelta = std::chrono::hours(24);
             auto nextWakeUp = std::chrono::system_clock::now() + wakeUpDelta;
 
-            while(true){
+            while (true)
+            {
                 std::this_thread::sleep_until(nextWakeUp);
                 nextWakeUp += wakeUpDelta;
                 LOG(INFO) << "About to rotate log file!";
                 auto L = el::Loggers::getLogger("default");
-                if(L == nullptr)LOG(ERROR)<<"Oops, it is not called default!";
-                else L->reconfigure();
+                if (L == nullptr)
+                    LOG(ERROR) << "Oops, it is not called default!";
+                else
+                    L->reconfigure();
             }
-
         });
 
         logRotatorThread.detach();
     };
 };
-
 
 #endif //FLEXIBLECOMPUTERLANGUAGE_LOGGER_H
