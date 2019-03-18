ec9abf3ceaf08c50e2232d3c32b40bdfbd932c12
murtazasmart <murtazasmart@hotmail.com>
2019-03-18 12:41:02 +0530

    Multiple OTP parsing implemented

--- FlexibleComputerLanguage/OTPParser.cpp
+++ FlexibleComputerLanguage/OTPParser.cpp
@@ -103,86 +103,97 @@ Node *OTPParser::OTPJSONToNodeTree(std::string otpsString)
     otps.Parse(otpsString.c_str());
     Node *root = MemoryManager::Inst.CreateNode(++id);
     int i = 0, j = 0, k = 0;
+    int otpCount = 0;
 
-    for (rapidjson::Value::ConstMemberIterator tp = otps[0].MemberBegin(); tp != otps[0].MemberEnd(); ++tp)
+//    for (rapidjson::Value::ConstMemberIterator tp = otps[0].MemberBegin(); tp != otps[0].MemberEnd(); ++tp)
+    for(rapidjson::Value::ConstValueIterator itr = otps.Begin(); itr != otps.End(); ++itr)
     {
-        rapidjson::Value &tpjson = otps[0][tp->name.GetString()];
-        Node *tpnode = MemoryManager::Inst.CreateNode(++id);
-        Node *itemnode = MemoryManager::Inst.CreateNode(++id);
-        tpnode->SetValue((char *)tpjson["stageID"].GetString());
-        itemnode->SetRValue((char *)tpjson["item"]["itemID"].GetString());
-        itemnode->SetLValue((char *)tpjson["item"]["itemName"].GetString());
-        tpnode->SetCustomObj(itemnode);
-        root->AppendNode(tpnode);
-        for (rapidjson::Value::ConstValueIterator tdp = tpjson["traceabilityDataPackets"].Begin(); tdp != tpjson["traceabilityDataPackets"].End(); ++tdp)
+        Node *otpNode = MemoryManager::Inst.CreateNode(++id);
+        otpCount++;
+        otpNode->SetValue("otp");
+        otpNode->SetLValue("otp");
+        otpNode->SetRValue("otp");
+        root->AppendNode(otpNode);
+        for(rapidjson::Value::ConstMemberIterator tp = itr->MemberBegin(); tp != itr->MemberEnd(); ++tp)
         {
-            rapidjson::Value &tdpjson = (rapidjson::Value&)(*tdp);
-            Node *tdpnode = MemoryManager::Inst.CreateNode(++id);
-            tdpnode->SetValue((char *)tdpjson["userID"].GetString());
-            tpnode->AppendNode(tdpnode);
-            for (rapidjson::Value::ConstValueIterator td = tdpjson["traceabilityData"].Begin(); td != tdpjson["traceabilityData"].End(); ++td)
+            rapidjson::Value &tpjson = (rapidjson::Value&)(itr->GetObject()[tp->name.GetString()]);
+            Node *tpnode = MemoryManager::Inst.CreateNode(++id);
+            Node *itemnode = MemoryManager::Inst.CreateNode(++id);
+            tpnode->SetValue((char *)tpjson["stageID"].GetString());
+            itemnode->SetRValue((char *)tpjson["item"]["itemID"].GetString());
+            itemnode->SetLValue((char *)tpjson["item"]["itemName"].GetString());
+            tpnode->SetCustomObj(itemnode);
+            otpNode->AppendNode(tpnode);
+            for (rapidjson::Value::ConstValueIterator tdp = tpjson["traceabilityDataPackets"].Begin(); tdp != tpjson["traceabilityDataPackets"].End(); ++tdp)
             {
-                rapidjson::Value &tdjson = (rapidjson::Value&)(*td);
-                Node *tdnode = MemoryManager::Inst.CreateNode(++id);
-                tdpnode->AppendNode(tdnode);
-                tdnode->SetValue((char *)tdjson["key"].GetString());
-                //                tdnode->SetValue((char *)"something is better");
-                if (tdjson["val"].IsObject() || tdjson["val"].IsArray())
+                rapidjson::Value &tdpjson = (rapidjson::Value&)(*tdp);
+                Node *tdpnode = MemoryManager::Inst.CreateNode(++id);
+                tdpnode->SetValue((char *)tdpjson["userID"].GetString());
+                tpnode->AppendNode(tdpnode);
+                for (rapidjson::Value::ConstValueIterator td = tdpjson["traceabilityData"].Begin(); td != tdpjson["traceabilityData"].End(); ++td)
                 {
-                    rapidjson::Value &val = (rapidjson::Value &)tdjson["val"];
-                    createTDTree(val, tdnode);
-                }
-                else if (tdjson["val"].IsBool())
-                {
-                    PString pStr = 0;
-                    MemoryManager::Inst.CreateObject(&pStr);
-                    bool val = tdjson["val"].GetBool();
-                    std::string val_bool = (val ? "true" : "false");
-                    pStr->SetValue(val ? "true" : "false");
-                    tdnode->SetEntityObj((PENTITY)pStr);
-                    tdnode->SetLValue((char *)val_bool.c_str());
-                }
-                else if (tdjson["val"].IsInt())
-                {
-                    PString pStr = 0;
-                    MemoryManager::Inst.CreateObject(&pStr);
-                    int val = tdjson["val"].GetInt();
-                    pStr->SetValue(std::to_string(val));
-                    tdnode->SetEntityObj((PENTITY)pStr);
-                    tdnode->SetLValue((char *)std::to_string(val).c_str());
-                }
-                else if (tdjson["val"].IsFloat())
-                {
-                    PString pStr = 0;
-                    MemoryManager::Inst.CreateObject(&pStr);
-                    float val = tdjson["val"].GetFloat();
-                    pStr->SetValue(std::to_string(val));
-                    tdnode->SetEntityObj((PENTITY)pStr);
-                    tdnode->SetLValue((char *)std::to_string(val).c_str());
-                }
-                else
-                {
-                    PString pStr = 0;
-                    MemoryManager::Inst.CreateObject(&pStr);
-                    std::string val = tdjson["val"].GetString();
-                    //                    std::replace(val.begin(), val.end(), '"', '\0');
-                    val.erase(std::remove(val.begin(), val.end(), '"'), val.end());
-                    pStr->SetValue(tdjson["val"].GetString());
-                    tdnode->SetEntityObj((PENTITY)pStr);
-                    tdnode->SetLValue((char *)val.c_str());
-                    //                    std::cout << ((PENTITY)tdnode->GetEntityObj())->ul_Type;
+                    rapidjson::Value &tdjson = (rapidjson::Value&)(*td);
+                    Node *tdnode = MemoryManager::Inst.CreateNode(++id);
+                    tdpnode->AppendNode(tdnode);
+                    tdnode->SetValue((char *)tdjson["key"].GetString());
+                    //                tdnode->SetValue((char *)"something is better");
+                    if (tdjson["val"].IsObject() || tdjson["val"].IsArray())
+                    {
+                        rapidjson::Value &val = (rapidjson::Value &)tdjson["val"];
+                        createTDTree(val, tdnode);
+                    }
+                    else if (tdjson["val"].IsBool())
+                    {
+                        PString pStr = 0;
+                        MemoryManager::Inst.CreateObject(&pStr);
+                        bool val = tdjson["val"].GetBool();
+                        std::string val_bool = (val ? "true" : "false");
+                        pStr->SetValue(val ? "true" : "false");
+                        tdnode->SetEntityObj((PENTITY)pStr);
+                        tdnode->SetLValue((char *)val_bool.c_str());
+                    }
+                    else if (tdjson["val"].IsInt())
+                    {
+                        PString pStr = 0;
+                        MemoryManager::Inst.CreateObject(&pStr);
+                        int val = tdjson["val"].GetInt();
+                        pStr->SetValue(std::to_string(val));
+                        tdnode->SetEntityObj((PENTITY)pStr);
+                        tdnode->SetLValue((char *)std::to_string(val).c_str());
+                    }
+                    else if (tdjson["val"].IsFloat())
+                    {
+                        PString pStr = 0;
+                        MemoryManager::Inst.CreateObject(&pStr);
+                        float val = tdjson["val"].GetFloat();
+                        pStr->SetValue(std::to_string(val));
+                        tdnode->SetEntityObj((PENTITY)pStr);
+                        tdnode->SetLValue((char *)std::to_string(val).c_str());
+                    }
+                    else
+                    {
+                        PString pStr = 0;
+                        MemoryManager::Inst.CreateObject(&pStr);
+                        std::string val = tdjson["val"].GetString();
+                        //                    std::replace(val.begin(), val.end(), '"', '\0');
+                        val.erase(std::remove(val.begin(), val.end(), '"'), val.end());
+                        pStr->SetValue(tdjson["val"].GetString());
+                        tdnode->SetEntityObj((PENTITY)pStr);
+                        tdnode->SetLValue((char *)val.c_str());
+                        //                    std::cout << ((PENTITY)tdnode->GetEntityObj())->ul_Type;
 
-                    //                    tdnode->SetValue((char *)tdjson["val"].dump().c_str());
-                }
-                //                std::cout << (char *)std::to_string(tdjson["type"].get<int>()).c_str();
+                        //                    tdnode->SetValue((char *)tdjson["val"].dump().c_str());
+                    }
+                    //                std::cout << (char *)std::to_string(tdjson["type"].get<int>()).c_str();
 //                std::cout << tdjson.HasMember("type");
-                if (tdjson.HasMember("type"))
-                {
-                    tdnode->SetRValue((char *)std::to_string(tdjson["type"].GetInt()).c_str());
-                }
-                else
-                {
-                    tdnode->SetRValue("90");
+                    if (tdjson.HasMember("type"))
+                    {
+                        tdnode->SetRValue((char *)std::to_string(tdjson["type"].GetInt()).c_str());
+                    }
+                    else
+                    {
+                        tdnode->SetRValue("90");
+                    }
                 }
             }
         }
